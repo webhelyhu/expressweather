5ccee3f673c990facb186905308b93d19e67bf81
Szy György <szygyuri@gmail.com>
2020-01-23 00:18:03 +0100

    minimum app-err.js file

--- app-err.js
+++ app-err.js
@@ -1,4 +1,16 @@
 const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.res.send({ info:'The test application is alive!' })
+  .listen(PORT, () => console.log(`Program is running. Listening on ${ PORT }`))
+
+/*
+
+
+const express = require('express')
 
 const app = express()  // generating the app.
 const port = process.env.PORT | 3000 // port for heroku, or if not exists, 3000
@@ -19,3 +31,4 @@ app.listen(port, () => {
     console.log('Program is alive. Server is up on port ' + port)
 })
 
+*/
